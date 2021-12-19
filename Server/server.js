0fb9be664976b97a10dade0a26e01fb3b828e91d
CSAapps <csasoftwares@gmail.com>
2021-12-19 19:47:07 +0530

    add orign to io

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,19 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "https://z53861ec8-z3e2c9533-gtw.qovery.io",
+    methods: ["GET", "POST"]
+  }
+});
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/port', (req, res) => {
+  res.send(port+"");
 });
 
 io.on('connection', (socket) => {
